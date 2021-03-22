0a8843727813c04cc28d80efb43e02f906d1300c
adrianbunea <adrian_bunea28@yahoo.com>
2021-03-22 22:08:50 +0200

    Dump Bootstrap and make modal using custom CSS

--- src/Pages/Menu.elm
+++ src/Pages/Menu.elm
@@ -1,32 +1,20 @@
 module Pages.Menu exposing (..)
 
-import Bootstrap.Button as Button
-import Bootstrap.Card as Card
-import Bootstrap.Card.Block as Block
-import Bootstrap.Form exposing (group)
-import Bootstrap.Form.Input as Input
-import Bootstrap.Grid as Grid
-import Bootstrap.Grid.Row as Row
-import Bootstrap.Modal as Modal
-import Bootstrap.Utilities.Border as Border
-import Bootstrap.Utilities.Display as Display
-import Bootstrap.Utilities.Flex as Flex
-import Bootstrap.Utilities.Size as Size
-import Bootstrap.Utilities.Spacing as Spacing
+import Components.Modal as Modal exposing (Config, defaultConfig)
 import Html exposing (..)
-import Html.Attributes exposing (for, required, style)
+import Html.Attributes exposing (class, for, required, style)
 import Html.Events exposing (onSubmit)
 
 
 type alias Model =
     { playerName : Maybe String
-    , modalInfo : ModalInfo
+    , formFields : FormFields
+    , modalConfig : Modal.Config Msg
     }
 
 
-type alias ModalInfo =
-    { playerName : String
-    , visibility : Modal.Visibility
+type alias FormFields =
+    { name : String
     }
 
 
@@ -42,19 +30,24 @@ init =
         playerName =
             ""
 
-        modalInfo =
-            { playerName = playerName
-            , visibility =
-                if playerName == "" then
-                    Modal.shown
+        modalConfig =
+            { defaultConfig
+                | isVisible =
+                    if playerName == "" then
+                        True
 
-                else
-                    Modal.hidden
+                    else
+                        False
+            }
+
+        formFields =
+            { name = ""
             }
 
         model =
             { playerName = Nothing
-            , modalInfo = modalInfo
+            , formFields = formFields
+            , modalConfig = modalConfig
             }
     in
     ( model, Cmd.none )
@@ -68,18 +61,19 @@ update msg model =
 
         ChangeName inputValue ->
             let
-                modalInfo =
-                    model.modalInfo
+                formFields =
+                    model.formFields
             in
-            ( { model | modalInfo = { modalInfo | playerName = inputValue } }, Cmd.none )
+            ( { model | formFields = { formFields | name = inputValue } }, Cmd.none )
 
         SaveName ->
             let
-                modalInfo =
-                    model.modalInfo
+                modalConfig =
+                    model.modalConfig
             in
-            ( { playerName = Just model.modalInfo.playerName
-              , modalInfo = { modalInfo | playerName = "", visibility = Modal.hidden }
+            ( { playerName = Just model.formFields.name
+              , formFields = model.formFields
+              , modalConfig = { modalConfig | isVisible = False }
               }
             , Cmd.none
             )
@@ -99,76 +93,22 @@ view model =
 
                 Nothing ->
                     text ""
+
+        modal =
+            model.modalConfig
+                |> Modal.view [ introForm model.formFields ]
     in
-    Grid.containerFluid [ Size.h100 ]
-        [ Grid.row [ Row.middleXs, Row.middleSm, Row.middleMd, Row.middleLg, Row.middleXl ]
-            [ Grid.col [] [ welcomeMessage, menuOptions, introModal model.modalInfo ]
-            ]
+    div [ class "container" ]
+        [ welcomeMessage
+        , modal
         ]
 
 
 menuOptions : Html Msg
 menuOptions =
-    Card.config
-        [ Card.attrs
-            [ Size.w75
-            , Spacing.mxAuto
-            , style "max-width" "40rem"
-            ]
-        ]
-        |> Card.headerH1 [] [ text "Main Menu" ]
-        |> Card.block [ Block.attrs [ Flex.block, Flex.col ] ]
-            [ Block.custom (Button.linkButton [] [ text "Play" ])
-            , Block.custom (Button.linkButton [] [ text "Settings" ])
-            ]
-        |> Card.view
+    text ""
 
 
-introModal : ModalInfo -> Html Msg
-introModal modalInfo =
-    Modal.config NoOp
-        |> Modal.centered True
-        |> Modal.body [ Border.all, Border.rounded ]
-            [ Grid.containerFluid []
-                [ Grid.row []
-                    [ Grid.col
-                        []
-                        [ introForm modalInfo.playerName
-                        ]
-                    ]
-                ]
-            ]
-        |> Modal.view modalInfo.visibility
-
-
-introForm : String -> Html Msg
-introForm modalName =
-    let
-        title =
-            h1 [ Spacing.mb5 ] [ text "Hi! First time? What's your name?" ]
-
-        nameGroup =
-            group []
-                [ label [ for "name" ] []
-                , Input.text
-                    [ Input.id "name"
-                    , Input.onInput ChangeName
-                    , Input.placeholder "Name"
-                    , Input.value modalName
-                    , Input.attrs [ required True, Spacing.mb4 ]
-                    ]
-                ]
-
-        submitButton =
-            Button.submitButton
-                [ Button.primary
-                , Button.large
-                , Button.attrs [ Spacing.mxAuto, Display.block ]
-                ]
-                [ text "Continue" ]
-    in
-    form [ onSubmit SaveName, Spacing.p4 ]
-        [ title
-        , nameGroup
-        , submitButton
-        ]
+introForm : FormFields -> Html Msg
+introForm { name } =
+    text "name"
